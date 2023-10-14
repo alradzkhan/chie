d7f472df3b1f02f957d8084497deb9514d8c39a8
alradzkhan <147899674+alradzkhan@users.noreply.github.com>
2023-10-14 12:39:13 +0800

    Update dbCon.php

--- connection/dbCon.php
+++ connection/dbCon.php
@@ -5,7 +5,7 @@ session_start();
 $host = "localhost";
 $username = "root";
 $password = "";
-$db_name = "samplefraskdb";
+$db_name = "samplechiedb";
 
 $con = mysqli_connect($host, $username, $password, $db_name);
 
